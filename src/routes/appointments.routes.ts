aa41197b3fe52b334fe645a08c828c48fcbd26bc
Rodrigo Costabrava <rodrigo.costabrava@cooperfenixti.com.br>
2020-05-14 17:02:31 -0300

    criando o models appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,14 +1,9 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/appointment';
 
 const appointmentsRouter = Router();
-
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
 
 const appointments: Appointment[] = [];
 
@@ -27,11 +22,7 @@ appointmentsRouter.post('/', (req, res) => {
       .json({ message: 'this appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
